5a2fe2e172a3e8adb5321aed4b019c2f9f23aa21
Borysenko Volodymyr <volodymyr.borysenko@kycaid.com>
2025-06-09 12:52:28 +0200

    tt events

--- src/common/constants/pixel.ts
+++ src/common/constants/pixel.ts
@@ -1,18 +1,22 @@
-export interface ICustomFBDataMap {
+export interface IFacebook {
   [key: string]: {
     currency: string;
     value: number;
     content_ids: string[];
     content_type: string;
+    content_id?: string;
   };
 }
 
-export interface ICustomTTDataMap {
+
+
+export interface ITikTok {
   [key: string]: {
     currency: string;
     value: number;
     content_type: string;
-    contents: [
+    // content_id?:number
+     contents?: [
       {
         price: number;
         quantity: number;
@@ -25,15 +29,7 @@ export interface ICustomTTDataMap {
   };
 }
 
-export interface ITTEventConversion {
-  [key: string]: {
-    currency: string;
-    value: number;
-    content_type: string;
-  };
-}
-
-export const CUSTOM_FB_DATA_MAP: ICustomFBDataMap = {
+export const FB: IFacebook = {
   PageView: {
     currency: 'USD',
     value: 0.01,
@@ -66,8 +62,13 @@ export const CUSTOM_FB_DATA_MAP: ICustomFBDataMap = {
   },
 };
 
-export const TT_EVENT_CONVERSION: ITTEventConversion = {
-  GenerateLead: {
+export const TT: ITikTok = {
+  PageView: {
+    currency: 'USD',
+    value: 0.01,
+    content_type: 'product',
+  },
+  Lead: {
     currency: 'USD',
     value: 0.04,
     content_type: 'product',
@@ -75,24 +76,6 @@ export const TT_EVENT_CONVERSION: ITTEventConversion = {
   ViewContent: {
     currency: 'USD',
     value: 0.1,
-    content_type: 'product',
-  },
-  Registration: {
-    currency: 'USD',
-    value: 2.0,
-    content_type: 'product',
-  },
-  Purchase: {
-    currency: 'USD',
-    value: 20.0,
-    content_type: 'product',
-  },
-};
-
-export const CUSTOM_TT_DATA_MAP: ICustomTTDataMap = {
-  PageView: {
-    currency: 'USD',
-    value: 0.01,
     content_type: 'product',
     contents: [
       {
@@ -104,65 +87,16 @@ export const CUSTOM_TT_DATA_MAP: ICustomTTDataMap = {
         brand: 'Buddy',
       },
     ],
-  },
-  Lead: {
-    currency: 'USD',
-    value: 0.04,
-    content_type: 'product',
-    contents: [
-      {
-        price: 0.04,
-        quantity: 1,
-        content_id: 'lead_123',
-        content_category: 'Game',
-        content_name: 'Lead Generation',
-        brand: 'Buddy',
-      },
-    ],
-  },
-  ViewContent: {
-    currency: 'USD',
-    value: 0.1,
-    content_type: 'product',
-    contents: [
-      {
-        price: 0.1,
-        quantity: 1,
-        content_id: 'view_123',
-        content_category: 'Game',
-        content_name: 'Content View',
-        brand: 'Buddy',
-      },
-    ],
+    
   },
   CompleteRegistration: {
     currency: 'USD',
     value: 2.0,
     content_type: 'product',
-    contents: [
-      {
-        price: 2.0,
-        quantity: 1,
-        content_id: 'reg_123',
-        content_category: 'Game',
-        content_name: 'Registration',
-        brand: 'Buddy',
-      },
-    ],
   },
   Purchase: {
     currency: 'USD',
     value: 20.0,
     content_type: 'product',
-    contents: [
-      {
-        price: 20.0,
-        quantity: 1,
-        content_id: 'offer_123',
-        content_category: 'Game',
-        content_name: 'Offer',
-        brand: 'Buddy',
-      },
-    ],
   },
 };
