f963f9f4822a894db6ce82b300db20b72c3f679b
Borysenko Volodymyr <volodymyr.borysenko@kycaid.com>
2026-02-05 17:31:38 +0100

    key param2

--- src/modules/segment/segment.service.ts
+++ src/modules/segment/segment.service.ts
@@ -101,7 +101,7 @@ export class SegmentService {
         event: this.KeitatoConvertionStatus(params.status),
         type: 'track',
         value: params.value || '0.1',
-        writeKey: this.segmentKey,
+        writeKey: params.key || this.segmentKey,
         UA:
           params.UA ||
           'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Mobile Safari/537.3',
